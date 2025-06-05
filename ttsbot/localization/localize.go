95a7f467b8fa10c0b5d5ddfecfee953769c6a9a3
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-06-05 09:23:15 +0900

    feat: implement GetOrGeneric method for resource retrieval

--- ttsbot/localization/localize.go
+++ ttsbot/localization/localize.go
@@ -17,6 +17,30 @@ func (r genericResources[S, T]) Get(locale S) (T, bool) {
 	return resource, ok
 }
 
+// This method returns the resource following order.
+//  1. If the locale exists, return it.
+//  2. If the locale does not exist, return the generic resource for the locale if it exists.
+//     For example, given a locale "en-US" but there is no resource for "en-US",
+//     then try to return the resource for "en" if it exists.
+//  3. If the generic resource does not exist, return no resource.
+//
+// TODO: profile this method to see if it is a bottleneck.
+// If it is, consider caching the generic resources in a map.
+func (r genericResources[S, T]) GetOrGeneric(locale S) (T, bool) {
+	resource, ok := r.Get(locale)
+	if ok {
+		return resource, true
+	}
+	genericLocale := S(strings.SplitN(string(locale), "-", 2)[0])
+	genericResource, ok := r.Get(genericLocale)
+	if ok {
+		return genericResource, true
+	}
+
+	var zero T
+	return zero, false
+}
+
 func (r genericResources[S, T]) Localizations(value func(resource T) string) map[S]string {
 	localizations := make(map[S]string, len(r))
 	for locale, resource := range r {
