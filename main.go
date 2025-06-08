8bb4c81724100c904c7183f0b413a54b67c0caed
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-06-08 14:01:19 +0900

    feat: implement temporary bot client to retrieve application ID for Redis session management

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgo/events"
 	"github.com/disgoorg/disgo/handler"
@@ -82,7 +83,47 @@ func main() {
 		}
 		slog.Info("Connected to Redis", slog.String("url", cfg.Redis.Url))
 
-		persistenceManager = session.NewPersistenceManager(b.Client.ApplicationID().String(), redisClient, 30*time.Second)
+		// not good way but we need to use application ID as identifier for Redis sessions...
+
+		readyChan := make(chan *events.Ready)
+		id, err := func() (string, error) {
+			tempClient, err := disgo.New(cfg.Bot.Token, bot.WithEventListenerChan(readyChan))
+			if err != nil {
+				slog.Error("Failed to create temporary bot client", slog.Any("err", err))
+				return "", fmt.Errorf("failed to create temporary bot client: %w", err)
+			}
+			defer func() {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				tempClient.Close(ctx)
+				slog.Info("Temporary bot client closed")
+			}()
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			if err := tempClient.OpenGateway(ctx); err != nil {
+				slog.Error("Failed to open temporary bot client gateway", slog.Any("err", err))
+				return "", fmt.Errorf("failed to open temporary bot client gateway: %w", err)
+			}
+			select {
+			case r := <-readyChan:
+				if r == nil {
+					slog.Error("Temporary bot client is nil")
+					return "", fmt.Errorf("temporary bot client is nil")
+				}
+				slog.Info("Temporary bot client is ready", slog.String("applicationID", r.Client().ApplicationID().String()))
+				return r.Client().ApplicationID().String(), nil
+			case <-ctx.Done():
+				slog.Error("Timed out waiting for temporary bot client to be ready")
+				return "", fmt.Errorf("timed out waiting for temporary bot client to be ready")
+			}
+		}()
+
+		if err != nil {
+			slog.Error("Failed to create temporary bot client", slog.Any("err", err))
+			os.Exit(-1)
+		}
+
+		persistenceManager = session.NewPersistenceManager(id, redisClient, 30*time.Second)
 		persistenceManager.StartHeartbeatLoop()
 		opts = append(opts, withCache(cache.New(&cache.Options{
 			Redis:      redisClient,
